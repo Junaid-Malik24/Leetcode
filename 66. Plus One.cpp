4d998fe8d37c864e44a948046dd31f3db2c17b3a
Junaid-Malik24 <jacob24august@gmail.com>
2024-09-19 01:30:46 +0500

    Plus One (2)

--- 66. Plus One.cpp
+++ 66. Plus One.cpp
@@ -1,5 +1,6 @@
 //leetcode: https://leetcode.com/problems/plus-one/
 
+//Approach 1(The short method)
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
@@ -16,3 +17,29 @@ public:
         return digits;
     }
 };
+//Approach 2 (The long method)
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int size = digits.size();
+        for (int i = size - 1; i >= 0; i--) {
+            if (digits[i] == 9 && i == 0) {
+                digits[i] = 0; 
+                digits.insert(digits.begin(), 1); 
+            }
+            else if (digits[i] != 9) {
+                digits[i] += 1; 
+                break;
+            }
+            else {
+                digits[i] = 0;
+                if (digits[i-1] != 9) { 
+                    digits[i-1] += 1;
+                    break;
+                }
+            }
+        }
+        return digits;
+    }
+};
+
