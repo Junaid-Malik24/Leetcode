933b880b8ffe320f845cb7749015a158dc99ac38
Junaid-Malik24 <jacob24august@gmail.com>
2024-10-07 11:56:28 +0500

    Permutations In String (1)

---
+++ 567. Permutation in String.cpp
@@ -0,0 +1,19 @@
+//leetcode : https://leetcode.com/problems/permutation-in-string/?envType=daily-question&envId=2024-10-07
+
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        if (s1.length() > s2.length()) return false;
+        vector<int> s1Count(26, 0), s2Count(26, 0);
+        for (int i = 0; i < s1.length(); ++i) {
+            s1Count[s1[i] - 'a']++;
+            s2Count[s2[i] - 'a']++;
+        }
+        for (int i = 0; i < s2.length() - s1.length(); ++i) {
+            if (s1Count == s2Count) return true;
+            s2Count[s2[i] - 'a']--;
+            s2Count[s2[i + s1.length()] - 'a']++;
+        }
+        return s1Count == s2Count;
+    }
+};
